0873ae9d2c4ceed95e72fd14c888c6780f0f6a24
Simon Van Der Weele (Personal) <simonvanderweelepersonal@Simons-MacBook-Pro.local>
2020-05-18 10:36:15 +0200

    Initialised Vue project using Parcel bundler
    Called api to test connection.

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Vue App bundled with Parcels {{message}}</h1>
+    <b-alert show>Welcome Alert</b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "testing data"
+    };
+  },
+  created(){
+      fetch('https://localhost:5001/api/games/');
+      console.log('created');
+  }
+};
+</script>
+
+<style>
+</style>
